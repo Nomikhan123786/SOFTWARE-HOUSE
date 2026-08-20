9090e99820fdf0a574ef8a5748fc7fe000e4ed47
Nomikhan123786 <nomank7887682@gmail.com>
2026-08-20 13:55:31 +0500

    Update the admin page and add the dns

--- backend/createAdmin/createAdmin.js
+++ backend/createAdmin/createAdmin.js
@@ -1,24 +1,29 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import connectDB from "../config/db";
-import User from "../models/User";
+import connectDB from "../config/db.js";
+import User from "../models/User.js";
+import dns from "dns";
 
-async () => {
+dotenv.config();
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+(async () => {
   await connectDB();
-  const email = (
-    process.env.ADMIN_EMAIL || "admin@gmail.com"
-  ).toLocaleLowerCase();
+  const email = (process.env.ADMIN_EMAIL || "admin@gmail.com").toLowerCase();
+
   const existing = await User.findOne({ email });
   if (existing) {
-    console.log("Admin already exists", email);
-    process.exist(0);
+    console.log("Admin already exists:", email);
+    process.exit(0);
   }
+
   const admin = await User.create({
     name: process.env.ADMIN_NAME || "Super Admin",
     email,
     password: process.env.ADMIN_PASSWORD || "Admin@12345",
     role: "admin",
   });
+
   console.log("Admin account created:");
   console.log("  Email:", admin.email);
   console.log("  Password:", process.env.ADMIN_PASSWORD || "Admin@12345");
@@ -26,4 +31,4 @@ async () => {
 
   await mongoose.disconnect();
   process.exit(0);
-};
+})();
